32295f54fb7ac43c789be7558e7b384f7b9267f9
Anna Cymanowska <Anna.Cymanowska@365.aplitt.pl>
2022-06-07 18:53:34 +0200

    brute force i kmp

--- SekretariatSzkoly/MainWindow.xaml.cs
+++ SekretariatSzkoly/MainWindow.xaml.cs
@@ -25,4 +25,88 @@ namespace SekretariatSzkoly
             InitializeComponent();
         }
     }
+    internal class Brutforce
+    {
+        public ICollection<char> Alphabet { get; set; }
+
+        private ICollection<string> _calculate(int lenght)
+        {
+            if (lenght <= 1) return Alphabet.Select(a => a + "").ToList();
+            ICollection<string> sub = _calculate(lenght - 1);
+            return (from alpha in Alphabet from prior in sub select alpha + prior).ToList();
+        }
+        public class KMPAlgorithm
+        {
+            
+
+            private static void preProcess(char[] p, int[] table, int n)
+            {
+                table[0] = 0;
+              
+                int i = 0, j = 1; 
+                while (j < n)
+                {
+                    if (p[j] == p[i])
+                    {
+                        table[j] = i + 1;
+                        i++; j++;
+                    }
+                    else
+                    {
+                        if (i != 0)
+                        {
+                            i = table[i - 1];
+                        }
+                        else
+                        {
+                            table[j] = 0;
+                            j++;
+                        }
+                    }
+                }
+            }
+            public static void main(String[] args)
+            {
+                char S[] = "abcaabcab".toCharArray();
+                char p[] = "abcab".toCharArray();
+                int table[] = new int[p.length];
+                preProcess(p, table, p.length);
+                int i = 0;
+                int j = 0;
+                boolean found = false; 
+
+                while (i < S.length)
+                {
+                    if (S[i] == p[j])
+                    {
+                       
+                        i++;
+                        j++;
+                    }
+                    else
+                    {
+                        if (j != 0) 
+                            j = table[j - 1];
+                        else
+                            i++;
+                    }
+                    if (j == p.length)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found)
+                    System.out.println("true");
+        else
+                    System.out.println("false");
+            }
+        }
+        public ICollection<string> Calculate(int lenght)
+        {
+            return Alphabet == null ? null : _calculate(lenght);
+        }
+    }
 }
+
